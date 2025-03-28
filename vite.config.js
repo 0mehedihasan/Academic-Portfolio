52f1cf8f0e21721f737dabcebe0bf66ec90dbcbb
Md. Mehedi Hasan <106642643+0mehedihasan@users.noreply.github.com>
2025-03-28 16:08:21 +0600

    Create vite.config.js

---
+++ vite.config.js
@@ -0,0 +1,11 @@
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+
+export default defineConfig({
+  plugins: [react()],
+  build: {
+    rollupOptions: {
+      input: './index.html', // Explicitly set the entry point
+    },
+  },
+});
